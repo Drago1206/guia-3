f0bc55082144095a29cdf060e2a79ab532472b0b
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-28 20:39:01 -0500

    3

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 addEventListener("DOMContentLoaded",()=>{
-    let personas=0,mujeres=0
-    while (personas <= 42){ 
-        mujeres += 1
-        let hombres = mujeres + 7  
-        personas = mujeres + hombres  
-    document.write(`El total de personas: ${personas}<br> `)
-    document.write(`La  mujer : ${mujeres}<br>`)
-    document.write(`Hombres: ${hombres}<br>`)
-    document.write(`_______________________________ <br>`)
+    let cont=0
+    while (cont<=9){
+        let n1=0,n2=0
+        n1=Number(prompt("Ingrese el primer numero: "))
+        n2=Number(prompt("Ingrese el segundo numero: "))
+        let sum=0,rest=0,div=0,mult=0
+        sum=(n1+n2)
+        rest=(n1-n2)
+        mult=(n1*n2)
+        div=(n1/n2)
+        cont+=1
+        document.write(`El total de la suma es de: ${sum}<br>`)
+        document.write(`El total de la resta es de: ${rest}<br>`)
+        document.write(`El total de la division  es de: ${div}<br>`)
+        document.write(`El total de la multiplicacion es de: ${mult}<br>`)
+        document.write(`______________________<br>`)
+    
+    
     }
-
-})
+    })
 
 
