c5df5f681d3c3f96b13f7bdb0bea5cf138ac203b
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-28 23:27:25 -0500

    7

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 addEventListener("DOMContentLoaded",()=>{
-    let distancia = 0 
-    let tiempo = 0
-while (distancia < 10000){
-    tiempo += 1
-    let A = (800 * tiempo) + (20*tiempo**2)/2 
-    let M = (800 * tiempo) + (10*tiempo**2)/2
-    distancia = Math.sqrt(A**2 + M**2)
-    document.write(`Segundo: ${tiempo}<br>`)
-document.write(`Distancia: ${Math.round(distancia,2)}<br>`)
-document.write(`___________________________________ <br>`)
+let sb=0,vend=0
+sb=Number(prompt("Ingrese el sueldo base: "))
+vend=Number(prompt("Ingrese la cantidad de vendedores: "))
+
+while(vend>0){
+    let v1=0,v2=0,v3=0
+    v1=Number(prompt("Ingrese el valor de la venta 1"))
+    v2=Number(prompt("Ingrese el valor de la venta 2"))
+    v3=Number(prompt("Ingrese el valor de la venta 3"))
+
+    let comision=0, comision2=0, comision3=0,acum=0
+    comision=v1*0.10
+    comision2=v2*0.10
+    comision3=v3*0.10
+    acum+=(comision+comision2+comision3)
+    vend-=1
+    let extra=sb+acum
+    document.write(`comision de la venta 1 ${comision}<br>`)
+    document.write(`comision de la venta 2 ${comision2}<br>`)
+    document.write(`comision3ision de la venta 3 ${comision3}<br>`)
+    document.write(`comision${acum}<br>`)
+    document.write(`El sueldo base es de:  ${sb}<br>`)
+    document.write(`el sueldo mas la comision es de:  ${extra}<br>`)
+    document.write(`______________<br>`)
 }
     })
 
