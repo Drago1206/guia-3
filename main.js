24f573be5004ecfd812fd20da0220457b6109848
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-03 17:12:07 -0500

    10

--- main.js
+++ main.js
@@ -1,43 +1,37 @@
-// #Diseñe un algoritmo que determine el resultado de la elección del representante estudiantil de la
-// #universidad X, para ello se presentaron tres candidatos A, B, y C.
-// #Para ganar la elección se debe obtener como mínimo el 51%.
-// #En caso que no haya un ganador se repite la elección en una segunda vuelta.
-// #Van a la segunda vuelta los dos candidatos que obtengan la más alta votación.
-// #Se anula la elección en caso de producirse un empate doble por el segundo lugar o un empate triple
+// #Se está realizando un estudio del tráfico en una de las calles de su ciudad. Para ello deberá controlar
+// #durante un cierto tiempo los vehículos que pasan por dicha calle.
+// #Los distintos tipos de vehículo será: Turismo, Autobús, Camión o Motocicleta. Además, si el tipo de
+// #vehículo es Turismo debe anotar el número de ocupantes.
+// #El programa deberá contar 200 vehículos, e ir guardando cuantos pasan de cada tipo.
+// #Finalmente mostrará el porcentaje de cada uno de los tipos.
 
 addEventListener("DOMContentLoaded",()=>{
-let ganador = 0,contA = 0,contB = 0,contC = 0
-let cvotantes=Number(prompt("Ingrese la cantidad de votantes"))
-while (ganador != 1){
-    for(let i; i<cvotantes;i++){
-        let vote=Number(prompt("Elije (1)Candidato A, (2)Candidato B, (3) Candidato C"))
-        if(vote==1){
-            contA+=1
-        }if(vote==2){
-            contB+=1
-        }if(vote==3){
-            contC+=1
-        }
-        if(contA>contB && contA>contC)
-        {document.write(`El candidato A gano con: ${contA}votos`)
-        ganador+=1
-        }if(contB>contA && contB>contC)
-        {document.write(`El ganador es el candidato B con: ${contB}votos.`)
-        ganador+=1
-        }if(contC>contA && contC>contB){
-            document.write(`El ganador es el candidato C con: ${contC}votos`)
-            ganador+=1
-        }
-        if(contA==contB){
-            document.write(`Empate entre A y B, se dara la segunda vuelta`)
-        }if(contB==contC){
-            document.write(`Empate entre B y C, se dara la segunda vuelta: `)
-        }if(contA==contC){
-            document.write(`Empate entre A y C, se dara la segunda vuelta`)
-        }
+let i=0,M=0,CA=0,C=0,A=0,T=0
+while(i>=2){
+    i++
+let selec=Number(prompt(`Ingrese el peso del vehiculo: `))
+    if(selec<=250){
+        M+=1
+        let porcentm=((M/M)*100)
+        document.write(`El porcentaje total de motos es de: ${porcentm}`)
+    }ifelse(selec<=1020);{
+        T+=1
+        let porcenT=((T/T)*100)
+        document.write(`El porcentaje total de Turismo es de: ${porcenT}`)
+    }if(selec<=2000){
+        CA+=1
+        let porcenCA=((CA/CA)*100)
+        document.write(`El porcentaje total de carros es de: ${porcenCA}`)
+    }ifelse(selec<=15000);{
+    let porcenA=((A/A)*100)
+    document.write(`El porcentaje total de Autobuses es de: ${porcenA}`)
+    }if(selec<=25000){
+        C+=1
+        let porcenC=((C/C)*100)
+        document.write(`El porcentaje total de camiones es de: ${porcenC}`)
     }
-}
 
-    })
+    }
+})
 
 
