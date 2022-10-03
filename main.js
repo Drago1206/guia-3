d218160ddd88bd3d2ee2bca5ab97cdd71dfb0df2
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-03 13:37:34 -0500

    8

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
+// #Diseñar un pseudocódigo que calcule el promedio ponderado para alumno del ITT. El cálculo se hace
+// #de la siguiente forma:
+// #Se multiplica cada calificación por los créditos de cada materia
+// #El resultado anterior se suma con los resultados de todas las materias, por separado se suman los
+// #créditos de cada materia y finalmente se divide la suma de todas las materias por sus respectivos
+// #créditos, entre la suma de todos los créditos. (materias: Fundamentos, BD y ética).
+
 addEventListener("DOMContentLoaded",()=>{
-let sb=0,vend=0
-sb=Number(prompt("Ingrese el sueldo base: "))
-vend=Number(prompt("Ingrese la cantidad de vendedores: "))
+let i=0,est=0
+est=Number(prompt("Ingrese el numero de estudiantes que va a calificar: "))
+while(i<est){
+let calf=0,credf=0,calfbd=0,credbd=0,calfet=0,credet=0
+i++
+calf=Number(prompt(`Ingrese la calificacion de fundamentos: `))
+cred=Number(prompt(`Ingrese los creditos de fundamentos: `))
+cafbd=Number(prompt(`Ingrese la calificacion de BD: `))
+credbd=Number(prompt(`Ingrese los creditos de BD`))
+calfet=Number(prompt(`Ingrese la calificacion de Etica: `))
+credet=Number(prompt(`Ingrese los creditos de Etica: `))
 
-while(vend>0){
-    let v1=0,v2=0,v3=0
-    v1=Number(prompt("Ingrese el valor de la venta 1"))
-    v2=Number(prompt("Ingrese el valor de la venta 2"))
-    v3=Number(prompt("Ingrese el valor de la venta 3"))
+let r=(calf*cred*cafbd*credbd*calfet*credet)
+let sumcred=(cred+credbd+credet)
+let sum=(calf+cafbd+calfet)/sumcred
 
-    let comision=0, comision2=0, comision3=0,acum=0
-    comision=v1*0.10
-    comision2=v2*0.10
-    comision3=v3*0.10
-    acum+=(comision+comision2+comision3)
-    vend-=1
-    let extra=sb+acum
-    document.write(`comision de la venta 1 ${comision}<br>`)
-    document.write(`comision de la venta 2 ${comision2}<br>`)
-    document.write(`comision3ision de la venta 3 ${comision3}<br>`)
-    document.write(`comision${acum}<br>`)
-    document.write(`El sueldo base es de:  ${sb}<br>`)
-    document.write(`el sueldo mas la comision es de:  ${extra}<br>`)
-    document.write(`______________<br>`)
+document.write(`El estudiante No.${i}<br>`)
+document.write(`Tiene un promedio ponderado  de: ${sum} <br>`)
+document.write(`_____________________________________<br>`)
 }
+
+
     })
 
 
