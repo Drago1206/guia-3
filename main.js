058a4a273f6f20cb382abe986183c522d0fdfa02
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-03 16:22:00 -0500

    9

--- main.js
+++ main.js
@@ -1,32 +1,42 @@
-// #Diseñar un pseudocódigo que calcule el promedio ponderado para alumno del ITT. El cálculo se hace
-// #de la siguiente forma:
-// #Se multiplica cada calificación por los créditos de cada materia
-// #El resultado anterior se suma con los resultados de todas las materias, por separado se suman los
-// #créditos de cada materia y finalmente se divide la suma de todas las materias por sus respectivos
-// #créditos, entre la suma de todos los créditos. (materias: Fundamentos, BD y ética).
+// #Diseñe un algoritmo que determine el resultado de la elección del representante estudiantil de la
+// #universidad X, para ello se presentaron tres candidatos A, B, y C.
+// #Para ganar la elección se debe obtener como mínimo el 51%.
+// #En caso que no haya un ganador se repite la elección en una segunda vuelta.
+// #Van a la segunda vuelta los dos candidatos que obtengan la más alta votación.
+// #Se anula la elección en caso de producirse un empate doble por el segundo lugar o un empate triple
 
 addEventListener("DOMContentLoaded",()=>{
-let i=0,est=0
-est=Number(prompt("Ingrese el numero de estudiantes que va a calificar: "))
-while(i<est){
-let calf=0,credf=0,calfbd=0,credbd=0,calfet=0,credet=0
-i++
-calf=Number(prompt(`Ingrese la calificacion de fundamentos: `))
-cred=Number(prompt(`Ingrese los creditos de fundamentos: `))
-cafbd=Number(prompt(`Ingrese la calificacion de BD: `))
-credbd=Number(prompt(`Ingrese los creditos de BD`))
-calfet=Number(prompt(`Ingrese la calificacion de Etica: `))
-credet=Number(prompt(`Ingrese los creditos de Etica: `))
-
-let r=(calf*cred*cafbd*credbd*calfet*credet)
-let sumcred=(cred+credbd+credet)
-let sum=(calf+cafbd+calfet)/sumcred
-
-document.write(`El estudiante No.${i}<br>`)
-document.write(`Tiene un promedio ponderado  de: ${sum} <br>`)
-document.write(`_____________________________________<br>`)
+let ganador = 0,contA = 0,contB = 0,contC = 0
+let cvotantes=Number(prompt("Ingrese la cantidad de votantes"))
+while (ganador != 1){
+    for(let i; i<cvotantes;i++){
+        let vote=Number(prompt("Elije (1)Candidato A, (2)Candidato B, (3) Candidato C"))
+        if(vote==1){
+            contA+=1
+        }if(vote==2){
+            contB+=1
+        }if(vote==3){
+            contC+=1
+        }
+        if(contA>contB && contA>contC)
+        {document.write(`El candidato A gano con: ${contA}votos`)
+        ganador+=1
+        }if(contB>contA && contB>contC)
+        {document.write(`El ganador es el candidato B con: ${contB}votos.`)
+        ganador+=1
+        }if(contC>contA && contC>contB){
+            document.write(`El ganador es el candidato C con: ${contC}votos`)
+            ganador+=1
+        }
+        if(contA==contB){
+            document.write(`Empate entre A y B, se dara la segunda vuelta`)
+        }if(contB==contC){
+            document.write(`Empate entre B y C, se dara la segunda vuelta: `)
+        }if(contA==contC){
+            document.write(`Empate entre A y C, se dara la segunda vuelta`)
+        }
+    }
 }
-
 
     })
 
